ec4d42337abdfe705bb48f595e10d379fe701f2b
billingsley-john <johnbillingsley1996@yahoo.com>
2020-08-05 15:50:03 +0100

    added tests for sweep functions

---
+++ tests/test_SweepSplineShape.py
@@ -0,0 +1,75 @@
+
+import os
+import unittest
+from pathlib import Path
+
+import pytest
+
+from paramak import SweepSplineShape
+from paramak.utils import cut_solid, intersect_solid, union_solid
+
+class test_object_properties(unittest.TestCase):
+    def test_solid_constuction(self):
+        """checks that a SweepSplineShape solid can be created"""
+
+        test_shape = SweepSplineShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (50, 0),
+                (20, 200),
+                (50, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+        test_shape.create_solid()
+        
+        assert test_shape.solid is not None
+
+    def test_azimuth_placement_angle(self):
+        """creates SweepSplineShape solids with single and multiple azimuth
+            placement angles and checks the relative volumes are correct"""
+
+        test_shape_1 = SweepSplineShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (100, 0),
+                (50, 200),
+                (100, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+
+        test_shape_2 = SweepSplineShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (100, 0),
+                (50, 200),
+                (100, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ",
+            azimuth_placement_angle = [0, 90, 180, 270]
+        )
+
+        assert test_shape_2.volume == pytest.approx(test_shape_1.volume * 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_SweepStraightShape.py
@@ -0,0 +1,230 @@
+
+import os
+import unittest
+from pathlib import Path
+
+import pytest
+
+from paramak import SweepStraightShape
+from paramak.utils import cut_solid, intersect_solid, union_solid
+
+
+class test_object_properties(unittest.TestCase):
+    def test_solid_constuction(self):
+        """checks that a SweepStraightShape solid can be created"""
+
+        test_shape = SweepStraightShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (50, 0),
+                (20, 200),
+                (50, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+        test_shape.create_solid()
+        
+        assert test_shape.solid is not None
+
+    def test_relative_solid_volumes(self):
+        """creates swept solids with different cross-sections and paths and checks that
+                their relative volumes are correct"""
+
+        test_shape_1 = SweepStraightShape(
+            points = [
+                (-10, 10),
+                (10, 10),
+                (10, -10),
+                (-10, -10)
+            ],
+            path_points = [
+                (50, 0),
+                (20, 200),
+                (50, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+
+        test_shape_2 = SweepStraightShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (50, 0),
+                (20, 200),
+                (50, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+
+        assert test_shape_2.volume == pytest.approx(test_shape_1.volume * 4)
+
+        test_shape_1 = SweepStraightShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (100, 0),
+                (75, 100),
+                (50, 200),
+                (50, 300),
+                (50, 400),
+                (75, 500),
+                (100, 600)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+
+        test_shape_2 = SweepStraightShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (100, 0),
+                (75, 100),
+                (50, 200),
+                (50, 300)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+
+        assert test_shape_1.volume == pytest.approx(test_shape_2.volume * 2, rel=0.01)   # within 1%
+
+
+    def test_cut_volume(self):
+        """creates two swept solids and cuts one from the other and checks that the volume
+                of the resultant solid is correct"""
+
+        inner_shape = SweepStraightShape(
+            points = [
+                (-10, 10),
+                (10, 10),
+                (10, -10),
+                (-10, -10)
+            ],
+            path_points = [
+                (100, 0),
+                (75, 100),
+                (50, 200),
+                (75, 300),
+                (100, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+
+        outer_shape = SweepStraightShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (100, 0),
+                (75, 100),
+                (50, 200),
+                (75, 300),
+                (100, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+
+        outer_shape_with_cut = SweepStraightShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (100, 0),
+                (75, 100),
+                (50, 200),
+                (75, 300),
+                (100, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ",
+            cut = inner_shape
+        )
+
+        assert outer_shape_with_cut.volume == pytest.approx(outer_shape.volume - inner_shape.volume)
+
+    def test_workplane_check(self):
+        """creates a SweepStraightShape with workplane = path_workplane and checks that a ValueError is raised"""
+
+        with pytest.raises(ValueError) as exe_info:
+            shape = SweepStraightShape(
+                points = [
+                    (-10, 10), (10, 10), (10, -10), (-10, -10)
+                ],
+                path_points = [
+                    (50, 0), (0, 200), (50, 400)
+                ],
+                workplane = "XY",
+                path_workplane = "XY"
+            )
+
+    def test_azimuth_placement_angle(self):
+        """creates SweepStraightShape solids with single and multiple azimuth
+            placement angles and checks the volumes are correct"""
+
+        test_shape_1 = SweepStraightShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (100, 0),
+                (50, 200),
+                (100, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ"
+        )
+
+        test_shape_2 = SweepStraightShape(
+            points = [
+                (-20, 20),
+                (20, 20),
+                (20, -20),
+                (-20, -20)
+            ],
+            path_points = [
+                (100, 0),
+                (50, 200),
+                (100, 400)
+            ],
+            workplane = "XY",
+            path_workplane = "XZ",
+            azimuth_placement_angle = [0, 90, 180, 270]
+        )
+
+        assert test_shape_2.volume == pytest.approx(test_shape_1.volume * 4)
+
+
+if __name__ == "__main__":
+    unittest.main()
